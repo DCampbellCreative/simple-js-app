55abc267f95424327b3b586146b8fc9af7a31036
DCampbellCreative <dougcampbell1990@gmail.com>
2021-08-03 19:38:01 -0400

    set variables for eevee and evolutions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,55 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+let pokemon1 = [
+  name: 'Eevee'
+  height: 0.3
+  type: 'normal'
+  ]
+
+  let pokemon2 = [
+    name: 'Vaporeaon'
+    height: 1
+    type: 'water'
+  ]
+
+  let pokemon3 = [
+    name: 'Jolteon'
+    height: 0.8
+    type: 'electric'
+  ]
+
+  let pokemon4 = [
+    name: 'Flareon'
+    height: 0.9
+    type: 'fire'
+  ]
+
+  let pokemon5 = [
+    name: 'Espeon'
+    height: 0.9
+    type: 'psychic'
+  ]
+
+  let pokemon6 = [
+    name: 'Umbreon'
+    height: 1
+    type: 'dark'
+  ]
+
+  let pokemon7 = [
+    name: 'Leafeon'
+    height: 1
+    type: 'grass'
+  ]
+
+  let pokemon8 = [
+    name: 'Glaceon'
+    height: 0.8
+    type: 'ice'
+  ]
+
+  let pokemon9 = [
+    name: 'Sylveon'
+    height: 1
+    type: 'fairy'
+  ]
