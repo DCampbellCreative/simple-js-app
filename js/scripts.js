0cda90483d0065a2914f5e5c777f101b86ef0b94
DCampbellCreative <dougcampbell1990@gmail.com>
2021-08-04 15:34:36 -0400

    add commas for correct syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,47 @@
 let pokemonList = [
   {
-    name: 'Eevee'
-    height: 0.3
+    name: 'Eevee',
+    height: 0.3,
     type: ['normal']
   },
   {
-    name: 'Vaporeaon'
-    height: 1
+    name: 'Vaporeaon',
+    height: 1,
     type: ['water']
   },
   {
-    name: 'Jolteon'
-    height: 0.8
+    name: 'Jolteon',
+    height: 0.8,
     type: ['electric']
   },
   {
-    name: 'Flareon'
-    height: 0.9
+    name: 'Flareon',
+    height: 0.9,
     type: ['fire']
   },
   {
-    name: 'Espeon'
-    height: 0.9
+    name: 'Espeon',
+    height: 0.9,
     type: ['psychic']
   },
   {
-    name: 'Umbreon'
-    height: 1
+    name: 'Umbreon',
+    height: 1,
     type: ['dark']
   },
   {
-    name: 'Leafeon'
-    height: 1
+    name: 'Leafeon',
+    height: 1,
     type: ['grass']
   },
   {
-    name: 'Glaceon'
-    height: 0.8
+    name: 'Glaceon',
+    height: 0.8,
     type: ['ice']
   },
   {
-    name: 'Sylveon'
-    height: 1
+    name: 'Sylveon',
+    height: 1,
     type: ['fairy']
   },
 ];
