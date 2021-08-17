82ddb05bb0fbf805736810be6cb23e7b1e250392
DCampbellCreative <dougcampbell1990@gmail.com>
2021-08-17 18:24:13 -0400

    style css, add function to add and display pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,58 @@
 let pokemonRepository = (function () {
-let pokemonList = [
-  {
-    name: 'Eevee',
-    height: 0.3,
-    type: ['normal']
-  },
-  {
-    name: 'Vaporeaon',
-    height: 1,
-    type: ['water'],
-  },
-  {
-    name: 'Jolteon',
-    height: 0.8,
-    type: ['electric']
-  },
-  {
-    name: 'Flareon',
-    height: 0.9,
-    type: ['fire']
-  },
-  {
-    name: 'Espeon',
-    height: 0.9,
-    type: ['psychic']
-  },
-  {
-    name: 'Umbreon',
-    height: 1,
-    type: ['dark']
-  },
-  {
-    name: 'Leafeon',
-    height: 1,
-    type: ['grass']
-  },
-  {
-    name: 'Glaceon',
-    height: 0.8,
-    type: ['ice']
-  },
-  {
-    name: 'Sylveon',
-    height: 1,
-    type: ['fairy']
-  },
-];
+  let pokemonList = [
+    {
+      name: 'Eevee',
+      height: 0.3,
+      type: ['normal']
+    },
+    {
+      name: 'Vaporeaon',
+      height: 1,
+      type: ['water'],
+    },
+    {
+      name: 'Jolteon',
+      height: 0.8,
+      type: ['electric']
+    },
+    {
+      name: 'Flareon',
+      height: 0.9,
+      type: ['fire']
+    },
+    {
+      name: 'Espeon',
+      height: 0.9,
+      type: ['psychic']
+    },
+    {
+      name: 'Umbreon',
+      height: 1,
+      type: ['dark']
+    },
+    {
+      name: 'Leafeon',
+      height: 1,
+      type: ['grass']
+    },
+    {
+      name: 'Glaceon',
+      height: 0.8,
+      type: ['ice']
+    },
+    {
+      name: 'Sylveon',
+      height: 1,
+      type: ['fairy']
+    },
+    {
+    name: 'Pikachu',
+    height: .3,
+    type: ['electric'],
+  }
+  ];
 
-function add(pokemon) {
+  function add(pokemon) {
     pokemonList.push(pokemon);
   }
 
@@ -55,20 +60,32 @@ function add(pokemon) {
     return pokemonList;
   }
 
+//adds pokemon to pokedex
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon_list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.classList.add('pokemon_button');
+    button.innerText = pokemon.name;
+    addEventListener("click", showDetails);
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon.name);
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
   };
 })();
 
-console.log(pokemonRepository.getAll());
 
-//gets names of pokemon from pokemonRepository and lists pokemon and thier heights and highlights small pokemon
+//displays pokemon in respository on DOM
 pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height < 1) {
-    document.write(pokemon.name + ' (Height: ' + pokemon.height + ')' + ' ' + pokemon.name + ' is tiny! ' + '<br>' )
-  }
-  else {
-    document.write(pokemon.name + ' (Height: ' + pokemon.height + ')' + '<br>')
-  }
+  pokemonRepository.addListItem(pokemon)
 });
